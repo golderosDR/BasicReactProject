5320c59afede527ba947f15164ffeaa664328e8d
golderosDR <golderos.spr@gmail.com>
2023-08-01 09:14:06 +0200

    added Homework23

---
+++ src/BoredAPI/BoredAPI.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+export interface BoredAPIData {
+    activity:string;
+    type: string;
+    participants:number;
+    price: number;
+    link:string;
+    accessibility:number;
+}
+
+export default function BoredAPI() : JSX.Element{
+
+    const boredAPIUrl:string='https://www.boredapi.com/api/activity';
+
+let activityData :BoredAPIData  = {
+    activity :"",
+    type :"",
+    participants:0,
+    price:0,
+    link:"",
+    accessibility:0,
+}
+
+    const[activityTODO,setActivity] = useState(activityData)
+    function getActivity():void{
+        fetch(boredAPIUrl)
+        .then((res:Response)=>res.json())
+        .then((resultObj) =>{
+            console.log(resultObj);
+            setActivity(resultObj); 
+        }        
+        );
+    }
+
+   
+    useEffect(()=>{getActivity()},[]);
+
+    return(
+    <>
+    <h1>Activity: {activityTODO.activity}</h1>
+    <div>Type: {activityTODO.type}</div>
+    <div>Participants: {activityTODO.participants}</div>
+    <div>Price: {activityTODO.price}</div>
+    <a href= {activityTODO.link}>{activityTODO.link}</a>
+    <div>Acessibility: {activityTODO.accessibility}</div>
+    </>)
+}
--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import PetObjects from './Pet/PetObjects'
-import React from 'react';
+import PetObjects from './Pet/PetObjects';
+import BoredAPI from './BoredAPI/BoredAPI';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-<PetObjects/>
+  <BoredAPI />
+{/* <PetObjects/> */}
  </>
 );
 
